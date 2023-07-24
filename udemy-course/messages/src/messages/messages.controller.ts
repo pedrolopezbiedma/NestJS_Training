329eb7a4332588138bc5c88dfbd98b82a15f309d
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-07-24 16:38:14 +0200

    Lesson 13

--- udemy-course/messages/src/messages/messages.controller.ts
+++ udemy-course/messages/src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  getMessages(): string[] {
+    return ['Message 1', 'Message 2'];
+  }
+
+  @Get(':id')
+  getMessage(messageId: number): string {
+    return 'Message 1';
+  }
+
+  @Post()
+  createMessage(message): string {
+    return 'Message created';
+  }
+}
