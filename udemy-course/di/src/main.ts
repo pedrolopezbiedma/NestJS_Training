0f4653b02389ef1f7f20c4ba845ea51321db012c
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-09-04 16:27:55 +0200

    Lesson 33

--- udemy-course/di/src/main.ts
+++ udemy-course/di/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Commented out because there is no App module, more modules to be created in #33 ->
+  const app = await NestFactory.create(ComputerModule);
   await app.listen(3000);
 }
 bootstrap();
---
+++ udemy-course/di/src/power/power.service.ts
@@ -0,0 +1,5 @@
+export class PowerService {
+  supplyPower(power: number): void {
+    return console.log('The powerService is supplying: ', power);
+  }
+}
