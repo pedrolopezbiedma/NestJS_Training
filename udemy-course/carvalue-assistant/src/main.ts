35e5af8e6c44dadc3fc4b6f95a298181831bc4a5
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-09-25 22:38:03 +0200

    Lesson 78

--- udemy-course/carvalue-assistant/src/main.ts
+++ udemy-course/carvalue-assistant/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session'); // Incompatibility with NestJS Project.
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(
+    cookieSession({
+      keys: ['mysecretkey'],
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
