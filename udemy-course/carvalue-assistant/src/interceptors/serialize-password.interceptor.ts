de512c7cad286df35bebb4db231db66fae102661
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-09-25 12:11:56 +0200

    Lesson 67

--- udemy-course/carvalue-assistant/src/interceptors/serialize-password.interceptor.ts
+++ udemy-course/carvalue-assistant/src/interceptors/serialize-password.interceptor.ts
@@ -1,17 +1,20 @@
 import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 import { Observable, map } from 'rxjs';
-import { PlainUserDto } from 'src/users/dto/plain-user.dto';
 
 export class SerializePasswordInterceptor implements NestInterceptor {
-  // This interceptor is going to get executed in every request and is going to hide the users password
+  // The constructor receives the Dto we are going to use to transform the response to.
+  constructor(private dto: any) {}
+
+  // This interceptor is going to get executed in every request and
+  // is going transform is to the specified dto in the constructor.
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log('I am running before handle the request.');
 
     return next.handle().pipe(
       map((data) => {
         console.log('I am running before the request is sent out');
-        return plainToClass(PlainUserDto, data, {
+        return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
--- udemy-course/carvalue-assistant/src/users/users.controller.ts
+++ udemy-course/carvalue-assistant/src/users/users.controller.ts
@@ -12,41 +12,42 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PlainUserDto } from 'src/users/dto/plain-user.dto';
 import { SerializePasswordInterceptor } from 'src/interceptors/serialize-password.interceptor';
 
 @Controller('auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
-  @UseInterceptors(SerializePasswordInterceptor)
+  @UseInterceptors(new SerializePasswordInterceptor(PlainUserDto))
   @Post('/signup')
   signupUser(@Body() body: CreateUserDto) {
     console.log('El body es -->, ', body);
     return this.userService.signupUser(body.email, body.password);
   }
 
-  @UseInterceptors(SerializePasswordInterceptor)
+  @UseInterceptors(new SerializePasswordInterceptor(PlainUserDto))
   @Get('/:id')
   getUser(@Param('id') userId: number) {
     console.log('getUserById with id -->', userId);
     return this.userService.getUserById(userId);
   }
 
-  @UseInterceptors(SerializePasswordInterceptor)
+  @UseInterceptors(new SerializePasswordInterceptor(PlainUserDto))
   @Get()
   getUserByEmail(@Query('email') userEmail: string) {
     console.log('getUserByEmail with email -->', userEmail);
     return this.userService.getUserByEmail(userEmail);
   }
 
-  @UseInterceptors(SerializePasswordInterceptor)
+  @UseInterceptors(new SerializePasswordInterceptor(PlainUserDto))
   @Delete('/:id')
   removeUser(@Param('id') userId: number) {
     console.log('removeUSer with id -->', userId);
     return this.userService.removeUser(userId);
   }
 
-  @UseInterceptors(SerializePasswordInterceptor)
+  @UseInterceptors(new SerializePasswordInterceptor(PlainUserDto))
   @Patch('/:id')
   updateUser(@Param('id') userId: number, @Body() body: UpdateUserDto) {
     console.log('updateUser with id --> ', userId, ' and body -->', body);
