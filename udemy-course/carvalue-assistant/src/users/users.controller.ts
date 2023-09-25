967f6e8e84d8c33ba22584ee1c155e5f7fc883ef
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-09-25 11:34:06 +0200

    Lesson 58

--- udemy-course/carvalue-assistant/src/users/users.controller.ts
+++ udemy-course/carvalue-assistant/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -10,5 +10,17 @@ export class UsersController {
   signupUser(@Body() body: CreateUserDto) {
     console.log('El body es -->, ', body);
     return this.userService.signupUser(body.email, body.password);
+  }
+
+  @Get('/:id')
+  getUser(@Param('id') userId: number) {
+    console.log('getUserById con id -->', userId);
+    return this.userService.getUserById(userId);
+  }
+
+  @Get()
+  getUserByEmail(@Query('email') userEmail: string) {
+    console.log('getUserByEmail con email -->', userEmail);
+    return this.userService.getUserByEmail(userEmail);
   }
 }
